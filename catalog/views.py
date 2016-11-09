20dc69dfc986436e27c8ac2a117d128e4712f939
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-08 21:52:43 -0500

    fix(catalog): create global categories variable
    Since categories are predefined, there is no need to query
    database more than once.

--- catalog/views.py
+++ catalog/views.py
@@ -4,10 +4,11 @@ from flask import render_template
 from db_setup import Category, Item, User
 
 
+categories = session.query(Category).order_by(Category.name.desc())
+
 @app.route('/')
 @app.route('/catalog')
 def catalog():
-    categories = session.query(Category).order_by(Category.name.desc())
     items = session.query(Item).order_by(Item.created_at).limit(10)
     return render_template('catalog.html', categories=categories, items=items)
 
