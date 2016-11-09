eda51dedc106ffbddc0c4f9346d03c2f715d35cd
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-08 20:44:00 -0500

    feat(catalog): create db session object

---
+++ catalog/db.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from db_setup import Base
+
+# Connect to Database and create database session
+engine = create_engine('sqlite:///itemcatalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
