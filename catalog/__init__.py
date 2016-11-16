5f4de30aa0f3409438d5c13fa362b51cf349a444
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-16 14:38:37 -0500

    refactor(catalog): move app configuration

--- catalog/__init__.py
+++ catalog/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+import config
 
 app = Flask(__name__)
+app.secret_key = 'super_secret_key'
+app.config['GOOGLE_ID'] = config.GOOGLE_LOGIN_CLIENT_ID
+app.config['GOOGLE_SECRET'] = config.GOOGLE_LOGIN_CLIENT_SECRET
+app.debug = True
 
 
 import views
+import oauthlib
