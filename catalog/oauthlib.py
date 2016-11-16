ff73a5b5c14bb2349613300ed356d06a2de8bc09
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-16 18:29:17 -0500

    feat(catalog): update local User table

--- catalog/oauthlib.py
+++ catalog/oauthlib.py
@@ -3,6 +3,8 @@ import config
 
 from flask import Flask, redirect, url_for, session, request, jsonify
 from flask_oauthlib.client import OAuth
+from db import session as db
+from db_setup import User
 
 
 # app = Flask(__name__)
@@ -45,9 +47,35 @@ def authorized():
         )
     session['google_token'] = (resp['access_token'], '')
     session['user'] = google.get('userinfo').data
+
+    user_id = get_user_id(session)
     return redirect(url_for('catalog'))
 
 
 @google.tokengetter
 def get_google_oauth_token():
     return session.get('google_token')
+
+# User Helper Functions
+
+
+def create_user(session):
+    user = User(name=session['username']['name'], email=session['user'][
+                   'email'], picture=session['user']['picture'])
+    db.add(user)
+    db.commit()
+    user = db.query(User).filter_by(email=session['email']).one()
+    return user.id
+
+
+def get_user_id(session):
+    try:
+        email = session['user']['email']
+        user = db.query(User).filter_by(email=email).one()
+        return user.id
+    except:
+        try:
+            user_id = create_user(session)
+            return user_id
+        except:
+            return None
