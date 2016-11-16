8b2e737c68c65ce6a9a9c5b234f08134681f46b1
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-16 10:28:33 -0500

    chore(): create dev branch with all files

---
+++ catalog/config.py
@@ -0,0 +1,9 @@
+GOOGLE_LOGIN_CLIENT_ID = "954232220602-me59j84hb9enrejc0nt4i3bkd7hnf42v.apps.googleusercontent.com"
+GOOGLE_LOGIN_CLIENT_SECRET = "5zMs7LtC6TgNHas8AamTdmOH"
+
+OAUTH_CREDENTIALS={
+        'google': {
+            'id': GOOGLE_LOGIN_CLIENT_ID,
+            'secret': GOOGLE_LOGIN_CLIENT_SECRET
+        }
+}
