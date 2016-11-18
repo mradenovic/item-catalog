2dc0bdc92133180d30f53e9f2fe751b960f7a80a
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-17 23:07:43 -0500

    fix(catalog): move populate.py one folder up
    Populate the database from same folder where server.py is, so the
    db file is created properly.

--- populate.py
+++ populate.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from db_setup import Category, Base, Item, User
+from catalog.db_setup import Category, Base, Item, User
 
 engine = create_engine('sqlite:///itemcatalog.db')
 # Bind the engine to the metadata of the Base class so that the
