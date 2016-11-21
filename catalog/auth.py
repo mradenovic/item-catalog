5282ed3bad98c8779f45539f54b8a80a428cb026
Mischa Radenovic <miroslav.radenovic@gmail.com>
2016-11-21 10:49:32 -0500

    refactor(catalog): move google oauth config
    Move google OAuth configuration to config file.

--- catalog/auth.py
+++ catalog/auth.py
@@ -5,7 +5,7 @@ This module contains all authentication and authorization methods.
 '''
 
 from catalog import app
-import config
+from config import GOOGLE
 
 from flask import Flask, redirect, url_for, session, request, jsonify
 from flask import flash
@@ -18,19 +18,7 @@ from functools import wraps
 # app = Flask(__name__)
 oauth = OAuth(app)
 
-google = oauth.remote_app(
-    'google',
-    consumer_key=app.config.get('GOOGLE_ID'),
-    consumer_secret=app.config.get('GOOGLE_SECRET'),
-    request_token_params={
-        'scope': 'email'
-    },
-    base_url='https://www.googleapis.com/oauth2/v1/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-)
+google = oauth.remote_app('google', **GOOGLE)
 
 
 @app.route('/login')
--- catalog/config.py
+++ catalog/config.py
@@ -3,9 +3,15 @@
 GOOGLE_LOGIN_CLIENT_ID = "954232220602-me59j84hb9enrejc0nt4i3bkd7hnf42v.apps.googleusercontent.com"
 GOOGLE_LOGIN_CLIENT_SECRET = "5zMs7LtC6TgNHas8AamTdmOH"
 
-OAUTH_CREDENTIALS={
-        'google': {
-            'id': GOOGLE_LOGIN_CLIENT_ID,
-            'secret': GOOGLE_LOGIN_CLIENT_SECRET
-        }
+GOOGLE = {
+    'consumer_key': GOOGLE_LOGIN_CLIENT_ID,
+    'consumer_secret': GOOGLE_LOGIN_CLIENT_SECRET,
+    'request_token_params': {
+        'scope': 'email'
+    },
+    'base_url': 'https://www.googleapis.com/oauth2/v1/',
+    'request_token_url': None,
+    'access_token_method': 'POST',
+    'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+    'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
 }
